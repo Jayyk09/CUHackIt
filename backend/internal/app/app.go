aa573b127b1eb9c0b6831632175a69d6b661887d
ChristianHirschey <christian.hirschey@gmail.com>
2026-02-28 16:31:10 +0000

    fixed compiler errors so main.go runs

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Jayyk09/CUHackIt/cmd/server"
 	"github.com/Jayyk09/CUHackIt/config"
 	"github.com/Jayyk09/CUHackIt/internal/database"
+	"github.com/Jayyk09/CUHackIt/internal/routes"
 	"github.com/Jayyk09/CUHackIt/pkg/logger"
 )
 
@@ -27,7 +28,7 @@ func Run(cfg *config.Config) {
 	// Create basic router
 	r := http.NewServeMux()
 
-	routes.Setup(r, db, l)
+	routes.Setup(r, db, cfg)
 
 	if err := server.Start(cfg, r, l); err != nil {
 		l.Fatal("Server error: %v", err)
