3a813f788c03184b4d8b9d63c798317c51334ab1
Ryan Joesting <rmjoesting@crimson.ua.edu>
2026-02-28 09:54:17 -0600

    Add Gemini prompt constants for food categorization and recipe recommendations

---
+++ backend/services/gemini/prompts.go
@@ -0,0 +1,92 @@
+package gemini
+
+const CategorizerPrompt = `You are a product categorizer for a grocery. You will be given a JSON of food items. You are to categorize the given food item into one of the following groups that is most appropriate:
+
+PRODUCE
+MEAT
+SEAFOOD
+DAIRY
+BAKERY
+PANTRY
+FROZEN
+SNACKS
+BEVERAGES
+DELI
+SPECIALTY
+
+You are also to provide the estimate shelf life of the item if bought fresh from a grocery store. Respond in only JSON. Here is an example input:
+
+[
+	{"food_name": "Banana"}
+]
+
+The appropriate response would be:
+
+[
+	{"food_name": "Banana", "category": "PRODUCE", "shelf_life": 5}
+]
+
+Now your turn. The input is given below. Respond with only the appropriate JSON.`
+
+const RecipePrompt = `You are a reccomendation engine for providing recipe reccomendations for food that is about to spoil in a given pantry. The pantry has limited ingredients. You will provide recipe names given a list of the ingredients in the pantry. Respond with a possible recipe using the ingredients in a JSON format. Give preference to simple, effective meals that use mostly ingredients in the pantry and to ingredients that are about to spoil within the next three days, indicated by is_spoiled: true. An example is shown below.
+
+If pantry is a JSON object given as:
+
+[
+  {
+    "food_name": "Milk",
+    "quantity": 1,
+    "units": "gallon",
+    "is_spoiled": false
+  },
+  {
+    "food_name": "Eggs",
+    "quantity": 12,
+    "units": "count",
+    "is_spoiled": false
+  },
+  {
+    "food_name": "Bread",
+    "quantity": 1,
+    "units": "loaf",
+    "is_spoiled": true
+  },
+  {
+    "food_name": "Rice",
+    "quantity": 5,
+    "units": "lbs",
+    "is_spoiled": false
+  },
+  {
+    "food_name": "Pasta",
+    "quantity": 3,
+    "units": "boxes",
+    "is_spoiled": false
+  },
+  {
+    "food_name": "Canned Beans",
+    "quantity": 4,
+    "units": "cans",
+    "is_spoiled": false
+  },
+  {
+    "food_name": "Chicken Breast",
+    "quantity": 2,
+    "units": "lbs",
+    "is_spoiled": true
+  }
+]
+
+An example appropriate response might be:
+
+[
+  {
+    "recipe_name": "Scrambled Eggs with Grilled Chicken",
+    "ingredients": [
+      {"food_name": "Eggs", "quantity": 12, "units": "count", "is_spoiled": false},
+      {"food_name": "Chicken Breast", "quantity": 2, "units": "lbs", "is_spoiled": true}
+    ]
+  }
+]
+
+Now your turn. You will be provided with the pantry list. Respond with ONLY the JSON for appropriate recipe.`
